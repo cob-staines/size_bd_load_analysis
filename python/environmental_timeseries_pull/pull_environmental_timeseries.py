0c5fb6e6b469f52d806e83b7b92f7ed3f2cb108a
cob-staines <cob.staines@gmail.com>
2026-05-30 12:11:08 -0500

    update output paths

--- python/environmental_timeseries_pull/pull_environmental_timeseries.py
+++ python/environmental_timeseries_pull/pull_environmental_timeseries.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 # CHIRPS for all dates (GEE)
 results_chirps = gee_point.extract_timeseries(
-    sites="environmental_timeseries_pull/inputs/site_date_all_2026-03-24.csv",
+    sites="environmental_timeseries_pull/inputs/site_date_all_2026-04-22.csv",
     products=["chirps"],
     combined=True,
     outdir="environmental_timeseries_pull/outputs/",
@@ -19,7 +19,7 @@ results_chirps = gee_point.extract_timeseries(
 
 # CHIRTS tmax through 2016 (GEE)
 results_chirts_pre_2017 = gee_point.extract_timeseries(
-    sites="environmental_timeseries_pull/inputs/site_date_pre_2017_2026-03-24.csv",
+    sites="environmental_timeseries_pull/inputs/site_date_pre_2017_2026-04-22.csv",
     products=["chirts_tmax"],
     combined=True,
     outdir="environmental_timeseries_pull/outputs/",
@@ -31,21 +31,22 @@ results_chirts_pre_2017 = gee_point.extract_timeseries(
 # CHIRTS tmax and tmin 2017+ (ERA5)
 # tmax
 result_chirts_post_2017 = chirts_era5_extract.extract_timeseries(
-    sites="environmental_timeseries_pull/inputs/site_date_post_2017_2026-03-24.csv",
+    sites="environmental_timeseries_pull/inputs/site_date_post_2017_2026-04-22.csv",
     product="tmax",
     local_dir="environmental_timeseries_pull/chirts_era5/",
     combined=True,
     outdir="environmental_timeseries_pull/outputs/",
 )
 
+
 # combine outputs
-results_chirps = pd.read_csv("environmental_timeseries_pull/outputs/all_sites_combined_20260324T1801.csv",
+results_chirps = pd.read_csv("environmental_timeseries_pull/outputs/all_sites_combined_20260422T1531.csv",
                              parse_dates=["date"],
                              dtype={"site_name": str}).rename(columns={"chirps": "precip_mm"})
-results_chirts_pre_2017 = pd.read_csv("environmental_timeseries_pull/outputs/all_sites_combined_20260325T0933.csv",
+results_chirts_pre_2017 = pd.read_csv("environmental_timeseries_pull/outputs/all_sites_combined_20260422T1548.csv",
                              parse_dates=["date"],
                              dtype={"site_name": str}).rename(columns={"chirts_tmax": "tmax_c"})
-results_chirts_post_2017 = pd.read_csv("environmental_timeseries_pull/outputs/all_sites_combined_20260325T0943.csv",
+results_chirts_post_2017 = pd.read_csv("environmental_timeseries_pull/outputs/all_sites_combined_20260422T1554.csv",
                              parse_dates=["date"],
                              dtype={"site_name": str}).rename(columns={"tmax": "tmax_c"})
 
@@ -58,4 +59,4 @@ results_all = results_chirps.merge(results_chirts, on=["site_name", "date", "lat
 check_1 = results_all[results_all["precip_mm"].isna()]
 check_2 = results_all[results_all["tmax_c"].isna()]
 
-results_all.to_csv("environmental_timeseries_pull/outputs/all_sites_combined_chirps_chirts_2026-03-25.csv", index = False)
+results_all.to_csv("environmental_timeseries_pull/outputs/all_sites_combined_chirps_chirts_2026-04-22.csv", index = False)
